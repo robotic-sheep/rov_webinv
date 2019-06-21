59723651fc9f6ffa30304b23f2c1f8bdc31104fd
robotic-sheep <jennifersongyy@gmail.com>
2019-06-21 16:26:22 -0700

    Robot sheep rocks

---
+++ routes/player.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+
+module.exports = {
+    addPlayerPage: (req, res) => {
+        res.render('add-player.ejs', {
+            title: 'VCS MATE ROV'
+            ,message: ''
+        });
+    },
+    addPlayer: (req, res) => {
+        if (!req.files) {
+            return res.status(400).send("No files were uploaded.");
+        }
+
+        let message = '';
+        let item_name = req.body.item_name;
+        let location = req.body.location;
+        let position = req.body.position;
+        let number = req.body.number;
+        let item_details = req.body.item_details;
+        let uploadedFile = req.files.image;
+        let image_name = uploadedFile.name;
+        let fileExtension = uploadedFile.mimetype.split('/')[1];
+        image_name = item_details + '.' + fileExtension;
+
+        let usernameQuery = "SELECT * FROM `players` WHERE user_name = '" + item_details + "'";
+
+        db.query(usernameQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            if (result.length > 0) {
+                message = 'Item already exists';
+                res.render('add-player.ejs', {
+                    message,
+                    title: 'VCS MATE ROV'
+                });
+            } else {
+                // check the filetype before uploading it
+                if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
+                    // upload the file to the /public/assets/img directory
+                    uploadedFile.mv(`public/assets/img/${image_name}`, (err ) => {
+                        if (err) {
+                            return res.status(500).send(err);
+                        }
+                        // send the player's details to the database
+                        let query = "INSERT INTO `players` (item_name, location, position, number, image, user_name) VALUES ('" +
+                            item_name + "', '" + location + "', '" + position + "', '" + number + "', '" + image_name + "', '" + item_details + "')";
+                        db.query(query, (err, result) => {
+                            if (err) {
+                                return res.status(500).send(err);
+                            }
+                            res.redirect('/');
+                        });
+                    });
+                } else {
+                    message = "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
+                    res.render('add-player.ejs', {
+                        message,
+                        title: 'VCS MATE ROV'
+                    });
+                }
+            }
+        });
+    },
+    editPlayerPage: (req, res) => {
+        let playerId = req.params.id;
+        let query = "SELECT * FROM `players` WHERE id = '" + playerId + "' ";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.render('edit-player.ejs', {
+                title: 'Edit  Item'
+                ,player: result[0]
+                ,message: ''
+            });
+        });
+    },
+    editPlayer: (req, res) => {
+        let playerId = req.params.id;
+        let item_name = req.body.item_name;
+        let location = req.body.location;
+        let position = req.body.position;
+        let number = req.body.number;
+
+        let query = "UPDATE `players` SET `item_name` = '" + item_name + "', `location` = '" + location + "', `position` = '" + position + "', `number` = '" + number + "' WHERE `players`.`id` = '" + playerId + "'";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.redirect('/');
+        });
+    },
+    deletePlayer: (req, res) => {
+        let playerId = req.params.id;
+        let getImageQuery = 'SELECT image from `players` WHERE id = "' + playerId + '"';
+        let deleteUserQuery = 'DELETE FROM players WHERE id = "' + playerId + '"';
+
+        db.query(getImageQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            let image = result[0].image;
+
+            fs.unlink(`public/assets/img/${image}`, (err) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                db.query(deleteUserQuery, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/');
+                });
+            });
+        });
+    }
+};
